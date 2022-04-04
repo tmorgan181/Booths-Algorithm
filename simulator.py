eeff614240859387c8984e2675f5eaf7befaf5cb
Trenton Morgan <tpm7qq@mst.edu>
2022-04-04 17:40:05 -0500

    Initialized simulator file

---
+++ simulator.py
@@ -0,0 +1,8 @@
+# Trenton Morgan, Karl Lodholz 2022
+# simulator.py
+
+# This file implements a simulated Booth's algorithm and verifies the accuracy of the calculations.
+# Additionally, it tracks the number of iterations and number of addition and subtraction operations
+# that must be performed in both Booth's and extended Booth's in order to quantify their performances.
+
+# code here
