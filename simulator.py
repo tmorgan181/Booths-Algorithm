b87304151cf7a1440a907f58fb5f13ea9bdca137
Trenton Morgan <tpm7qq@mst.edu>
2022-04-07 21:12:57 -0500

    Began implementing Booth's algorithm function.

--- simulator.py
+++ simulator.py
@@ -1,10 +1,9 @@
 # Trenton Morgan, Karl Lodholz 2022
-# simulator.py
 
-# This file implements a simulated Booth's algorithm and verifies the accuracy
-# of the calculations. Additionally, it tracks the number of iterations and
-# number of addition and subtraction operations that must be performed in both
-# Booth's and extended Booth's in order to quantify their performances.
+# This file implements a simulated Booth's algorithm and tracks the number of
+# iterations and number of addition and subtraction operations that must be
+# performed in both Booth's and extended Booth's in order to quantify their
+# performances.
 
 
 # Sets of acceptable binary characters for error checking
@@ -29,9 +28,6 @@ def main() -> None:
 
 	Params:
 		None
-	
-	Outputs:
-		?
 
 	Return:
 		None
@@ -45,28 +41,55 @@ def main() -> None:
 		from_stdin: str = input("Use stdin? (y/n) ")
 		if from_stdin == "y" or from_stdin == "Y":
 			
+			# Get the inputs.
+			multiplicand: str = input(
+				"Input multiplicand (0s and 1s only, no prefix): ")
 			multiplier: str = input(
 				"Input multiplier (0s and 1s only, no prefix): ")
-			multiplicand: str = input(
-				"Input multiplicand (0s and 1s only, no prefix):")
 
+			# Check if the inputs are valid binary numbers.
 			x: set = set(multiplicand).union(set(multiplier))
 			if x == BIN_SET or x == ZERO_SET or x == ONE_SET:
 				
-				if (len(multiplicand) >= MIN_INPUT_LEN and
-				len(multiplicand) <= MAX_INPUT_LEN and
-				len(multiplier) >= MIN_INPUT_LEN and
-				len(multiplier) <= MAX_INPUT_LEN):
+				if (len(multiplicand) >= MIN_INPUT_LEN
+				and len(multiplicand) <= MAX_INPUT_LEN
+				and len(multiplier) >= MIN_INPUT_LEN
+				and len(multiplier) <= MAX_INPUT_LEN):
 				
+					# All checks passed, inputs are good.
 					print("Valid inputs given.")
 					valid_input = True
+
 				else:
 					print("Invalid input(s) given.")
 			else:
 				print("Invalid input(s) given.")
+
+		elif from_stdin == "n" or from_stdin == "N":
+			
+			# Preset values for now
+			multiplicand = "0011"
+			multiplier = "0010"
+
+			valid_input = True
+
 		else:
 			print("Invalid input option given (need to implement dataset).")
 
+	# Sanity check for inputs and product.
+	multiplicand_temp = "0b" + multiplicand
+	multiplier_temp = "0b" + multiplier
+	print("Multiplicand:", multiplicand)
+	print("Multiplier:", multiplier)
+	product: str = bin(int(multiplicand_temp, 2) * int(multiplier_temp, 2))
+	print("Product:", product[2:])
+
+	# Now we have the inputs, we can run Booths and Extended Booths.
+	default_result: str = Booths(multiplicand, multiplier)
+	extended_result: str = Booths(multiplicand, multiplier, extended = True)
+
+	print("Default Result:", default_result)
+	print("Extended Result:", extended_result)
 
 	return
 
@@ -82,16 +105,44 @@ def Booths(multiplicand: str, multiplier: str, extended: bool = False) -> str:
 		multiplier: The second binary number in the operation.
 		extended: Switch to enable Extended Booth's when value is 1.
 
-	Outputs:
-		The tracked number of iterations and additions.
-
 	Return:
-		product: The calculated product as a binary string.
+		product: The calculated product as a binary string, or an empty string
+			if an error occurs.
 	"""
+	
+	# Extended Booth's requires the multiplicand and multiplier to be odd length???
 
-	product: str = ""
+	# Initialize the upper half of the double register for the product.
+	dbl_reg_upper: str = ""
+	for i in range(len(multiplier)):
+		dbl_reg_upper += '0'
+	print("Upper dbl:", dbl_reg_upper)
+
+	# The final product is stored in the double register and extended by a '0'
+	# bit to the right.
+	product: str = dbl_reg_upper + multiplier + '0'
+	print("Product:", product)
+ 
+	# Now we are ready to execute Booth's or Extended Booth's.
+	if not extended:
+		pass
+		# Default Booth's:
+
+		# Repeat for the length of the multiplier.
+
+	else:
+		pass
+		# Extended Booth's:
+
+		# run extended
 
 	return product
+
+
+def Arithmetic_Shift_Right(bin_number: str) -> str:
+	shifted_number: str = ""
+
+	return shifted_number
 
 
 # Start the main function on script execution.
